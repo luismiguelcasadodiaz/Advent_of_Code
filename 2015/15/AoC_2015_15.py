bf35e9ad7eb9eacc6262c60a81b5a0f596670854
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2023-03-07 09:58:33 +0100

    Add calculate blend and blend generation

--- 2015/15/AoC_2015_15.py
+++ 2015/15/AoC_2015_15.py
@@ -38,9 +38,16 @@ A capacity of 44*-1 + 56*2 = 68
 A durability of 44*-2 + 56*3 = 80
 A flavor of 44*6 + 56*-2 = 152
 A texture of 44*3 + 56*-1 = 76
-Multiplying these together (68 * 80 * 152 * 76, ignoring calories for now) results in a total score of 62842880, which happens to be the best score possible given these ingredients. If any properties had produced a negative total, it would have instead become zero, causing the whole score to multiply to zero.
 
-Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you can make?
+Multiplying these together (68 * 80 * 152 * 76, ignoring calories for now) 
+results in a total score of 62842880, which happens to be the best score 
+possible given these ingredients. 
+
+If any properties had produced a negative total, it would have instead become 
+zero, causing the whole score to multiply to zero.
+
+Given the ingredients in your kitchen and their properties, what is the total 
+score of the highest-scoring cookie you can make?
 
 """
 import os
@@ -52,11 +59,63 @@ def get_data(filename)->list:
     for line in f:
       l=line.strip().split(":")
       d=dict(x.strip().split(" ") for x in l[1].split(","))
-      data.append([l[0],d])
-  return data
-  
-data=get_data("test.txt")
-print(data)
+      #convert dict str values into integer
+      for k,v in d.items():
+        d[k]=int(v)
 
-data=get_data("input.txt")
-print(data)
+      data.append([l[0],d])
+      
+  return data , len(data), len(data[0][1])
+
+def calculate_blend(teaspoons):
+  #create a dictionary for store operations. the dict ignores calories for now
+  properties={}
+  for k,v in ingredients[0][1].items():
+    if k != "calories":
+      properties.setdefault(k,0)
+    
+  for k in properties.keys():
+    for i,v in enumerate(ingredients):
+      #here calculate a capacity of 44*-1 + 56*2 = 68
+      properties[k] += teaspoons[i] * v[1][k]
+      # check if negative
+    if properties[k] <= 0:
+      properties[k] = 0
+  #Multiplying these together (68 * 80 * 152 * 76)
+  multiplication = 1
+  for k in properties.keys():
+    multiplication *= properties[k]
+  return multiplication
+
+
+if __name__ == "__main__":
+  
+  ingredients, numingredients, properties =get_data("test.txt")
+  print(ingredients, numingredients, properties)
+  
+  teaspoons=[44,56]
+  score=calculate_blend(teaspoons)
+  print("TEST:highest-scoring is {} made of this blend {}".format(score, teaspoons))
+  print("="*80)
+  
+  
+  
+  ingredients, numingredients, properties=get_data("input.txt")
+  print(ingredients, numingredients, properties)
+
+  
+  MaxScore=0
+  Maxteaspoons=[]
+  for s in range(100,0,-1):
+    for p in range (100-s,0,-1):
+      for f in range (100-s-p,0,-1):
+        for a in range(100 -s-p-f,0,-1):
+          if s + p + f + a == 100:
+            teaspoons=[s,p,f,a]
+            score=calculate_blend(teaspoons)
+            if score > MaxScore:
+              Maxscore = score
+              Maxteaspoons =teaspoons
+              
+  print("ONE:highest-scoring is {} made of this blend {}".format(Maxscore, Maxteaspoons))
+  print("="*80)
